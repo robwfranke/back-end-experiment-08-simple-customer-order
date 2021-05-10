3860fe1a787ef47d603a7d0451abb574568f8322
=Rob Franke <robwfranke@gmail.com>
2021-05-10 14:27:08 +0200

    alleen customer en order

--- src/main/java/nl/lotrac/bv/model/Customer.java
+++ src/main/java/nl/lotrac/bv/model/Customer.java
@@ -12,7 +12,7 @@ public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
 
 
     //    @Id
--- src/main/java/nl/lotrac/bv/repository/JobRepository.java
+++
@@ -1,7 +0,0 @@
-package nl.lotrac.bv.repository;
-
-import nl.lotrac.bv.model.Job;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobRepository extends JpaRepository <Job, String >{
-}
